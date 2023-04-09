d7ab3b82c2f7c9f04c1a458e6e676b9daa98adb0
WanKai <WanKai@100.com>
2023-04-09 15:31:37 +0800

    添加文件

---
+++ HelloGit/src/main/java/wk/HelloGit.java
@@ -0,0 +1,13 @@
+package wk;
+
+/**
+ * @author xxl
+ * @create 2023-04-09-15:00
+ * @Description
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+    }
+}
